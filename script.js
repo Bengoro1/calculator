4f2c2004de8f083f6fff2dec07b8799efbf5244e
Benjamin <benjamin.barvas@gmail.com>
2023-07-01 17:32:07 +0200

    eventListeners for buttons

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ function multiply(a, b) {
     return a * b;
 }
 
+let firstNumber = '';
+let secondNumber = '';
+let operator;
+
 function operate() {
     if(firstNumber !== '' && +input.textContent !== NaN && secondNumber !== '') {
         b = +secondNumber;
@@ -32,7 +36,7 @@ function operate() {
         } else if(operator === times) {
             secondNumber = multiply(a,b);
         }
-        result.textContent = secondNumber
+        result.textContent = `${a} ${operator.textContent} ${b} = \r ${secondNumber}` 
         firstNumber = secondNumber;
         secondNumber = '';
     } else if(firstNumber === '') {
@@ -42,3 +46,42 @@ function operate() {
     }  
     input.textContent = '';
 }
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        secondNumber += number.textContent;
+        input.textContent = secondNumber;
+    });
+});
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((opers) => {
+    opers.addEventListener('click', () => {
+        operate();
+        operator = opers;
+        result.textContent += ' ' + opers.textContent;
+    });
+});
+
+const equal = document.getElementById('equal');
+equal.addEventListener('click', () => {
+    if (firstNumber !== '' && secondNumber !== '') {
+        operate();
+    }
+});
+
+const del = document.getElementById('delete');
+del.addEventListener('click', () => {
+    input.textContent = input.textContent.slice(0, -1);
+    secondNumber = input.textContent;
+});
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', () => {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    input.textContent = '';
+    result.textContent = '';
+});
