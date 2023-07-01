d5eaec45234cb342ec9cb86d204eade6b432204f
Benjamin <benjamin.barvas@gmail.com>
2023-07-01 16:43:40 +0200

    function operate

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ function divide(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function operate() {
+    if(firstNumber !== '' && +input.textContent !== NaN && secondNumber !== '') {
+        b = +secondNumber;
+        a = +firstNumber;
+        if(operator === plus) {
+            secondNumber = add(a,b);
+        } else if(operator === minus) {
+            secondNumber = substract(a,b);
+        } else if(operator === divided) {
+            if(secondNumber === 0) {
+                result.textContent = 'Nice try!';
+                return null
+            } else {
+                secondNumber = divide(a,b);
+            }
+        } else if(operator === times) {
+            secondNumber = multiply(a,b);
+        }
+        result.textContent = secondNumber
+        firstNumber = secondNumber;
+        secondNumber = '';
+    } else if(firstNumber === '') {
+        result.textContent = secondNumber
+        firstNumber = secondNumber;
+        secondNumber = '';
+    }  
+    input.textContent = '';
+}
